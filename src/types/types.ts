ab616b207dce14f62650b1fb3bfcf748799936da
Georgii Tretiakov <george22021988@gmail.com>
2026-08-24 16:06:34 +0100

    Redefined task status, priority, visibility, and assignment status as const

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,12 +3,37 @@ export const ROLE = {
   User: "USER",
 } as const; // ROLE is a const object
 
-export type Role = (typeof ROLE)[keyof typeof ROLE]; // taking the key of that object and creating with them
+export const TASKSTATUS = {
+  Todo: "TODO",
+  In_progress: "IN_PROGRESS",
+  Done: "DONE",
+} as const;
 
-export type TaskStatus = "TODO" | "IN_PROGRESS" | "DONE";
-export type TaskPriority = "LOW" | "MEDIUM" | "HIGH";
-export type TaskVisibility = "ONLY_ME" | "LIST" | "ANYONE";
-export type AssignmentStatus = "NONE" | "PENDING" | "APPROVED" | "REJECTED";
+export const TASKPRIORITY = {
+  Low: "LOW",
+  Medium: "MEDIUM",
+  High: "HIGH",
+} as const;
+
+export const TASKVISIBILITY = {
+  Only_me: "ONLY_ME",
+  List: "LIST",
+  Anyone: "ANYONE",
+} as const;
+
+export const ASSIGNMENTSTATUS = {
+  None: "NONE",
+  Pending: "PENDING",
+  Approved: "APPROVED",
+  Rejected: "REJECTED",
+} as const;
+
+export type Role = (typeof ROLE)[keyof typeof ROLE]; // taking the key of that object and creating with them
+export type TaskStatus = (typeof TASKSTATUS)[keyof typeof TASKSTATUS];
+export type TaskPriority = (typeof TASKPRIORITY)[keyof typeof TASKPRIORITY];
+export type TaskVisibility = (typeof TASKVISIBILITY)[keyof typeof TASKVISIBILITY];
+export type AssignmentStatus = (typeof ASSIGNMENTSTATUS)[keyof typeof ASSIGNMENTSTATUS];
+  
 export interface UserRef {
   id: string;
   nickname: string;
