91c300022c7998eff3afaf1d1bef3612c346f8c1
Georgii Tretiakov <george22021988@gmail.com>
2026-09-11 12:19:32 +0100

    Update taskWorker.ts

--- src/Workers/taskWorker.ts
+++ src/Workers/taskWorker.ts
@@ -32,16 +32,16 @@ self.onmessage = (event: MessageEvent<WorkerRequest>) => {
     let result = 0;
     switch (sortBy) {
       case SORTTYPE.title:
-        result = a.title.localeCompare(b.title);
+        result = a.title.localeCompare(b.title); // ascii comparison
         break;
       case SORTTYPE.status:
-        result = statusOrder[a.status] - statusOrder[b.status];
+        result = statusOrder[a.status] - statusOrder[b.status]; // with order it sorts logically instead of alphabetically
         break;
       case SORTTYPE.priority:
         result = priorityOrder[a.priority] - priorityOrder[b.priority];
         break;
       case SORTTYPE.createdAt:
-        result = a.createdAt.getTime() - b.createdAt.getTime();
+        result = a.createdAt.getTime() - b.createdAt.getTime(); // getTime converts Date into a milliseconds
         break;
       case SORTTYPE.updatedAt:
         result = a.updatedAt.getTime() - b.updatedAt.getTime();
@@ -51,7 +51,7 @@ self.onmessage = (event: MessageEvent<WorkerRequest>) => {
   });
   self.postMessage(sortedTasks);
 };
-// with self.onmessage the worker receives the data from the main thread and with self.postMessage it sends back the result.
+// with self.onmessage the worker listens for React app (main thread) to send data and with self.postMessage it sends back the result.
 // Worker.postMessage (what to send) -> self.onmessage (what to execute)
 // self.postMessage (what to send) -> Worker.onmessage (what to execute)
 // messageEvent is created by the browser and contains the message that was sent (that has the shape of WorkerRequest). event.data is exactly that
