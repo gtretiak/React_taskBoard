1632433173335506b7e03074b9450e86f4ef4123
Georgii Tretiakov <george22021988@gmail.com>
2026-09-11 12:56:18 +0100

    Update clientAPI.ts

--- src/api/clientAPI.ts
+++ src/api/clientAPI.ts
@@ -6,7 +6,8 @@ const AUTH_KEY = "accessToken";
 
 interface ApiRequestOptions extends RequestInit {
   auth?: boolean;
-}
+} // extending any incoming HTTP request by adding auth which is a flag of whether a route requires logged-in user or not
+
 export async function apiRequest(
   endpoint: string,
   options?: ApiRequestOptions,
@@ -15,13 +16,13 @@ export async function apiRequest(
   const headers = new Headers(options?.headers);
   if (options?.auth && token) {
     headers.set("Authorization", `Bearer ${token}`);
-  }
+  } // injecting the token to the auth header
   const response = await fetch(`${baseUrl}${endpoint}`, {
     ...options,
     headers,
   }); // including Authorization header
   if (!response.ok) {
-    if (response.status === 401) useAuthStore.getState().invalidateSession();
+    if (response.status === 401) useAuthStore.getState().invalidateSession(); // 401 - token expired, fake or corrupted, so the expired session data gets removed
     const error = await response.json();
     throw new Error(`API Error: ${response.status}: ${error.message}`);
   }
