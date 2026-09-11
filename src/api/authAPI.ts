a332a75ce6ab23b1254be1a67c2296afc40183f8
Georgii Tretiakov <george22021988@gmail.com>
2026-09-11 13:05:35 +0100

    Update authAPI.ts

--- src/api/authAPI.ts
+++ src/api/authAPI.ts
@@ -6,6 +6,7 @@ import type {
 } from "../types/requestsTypes";
 import { apiRequest } from "./clientAPI";
 
+// async means UI is not locked while API function is being performed and until a response to a request doesn't come 
 export async function loginAPI(data: LoginRequest): Promise<AuthResponse> {
   const response = await apiRequest("/auth/login", {
     method: "POST",
@@ -16,6 +17,8 @@ export async function loginAPI(data: LoginRequest): Promise<AuthResponse> {
   });
   return response.json();
 }
+// JSON.stringify is serialization JSON into a string
+// JSON.parse is parsing a string back to a JSON object
 
 export async function registerAPI(data: RegisterRequest): Promise<void> {
   await apiRequest("/auth/register", {
