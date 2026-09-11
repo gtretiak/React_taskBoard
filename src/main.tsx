98acb42582e2ef071d1ffc2c54afc1a0e38c6465
Georgii Tretiakov <george22021988@gmail.com>
2026-09-11 11:43:29 +0100

    Improve comments in main.tsx for better understanding
    Updated comments for clarity and added notes on deprecated functions.

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import { StrictMode } from "react"; //check for depricated functions and accidental side effect
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
@@ -15,5 +15,5 @@ createRoot(document.getElementById("root")!).render(
     </BrowserRouter>
   </StrictMode>,
 );
-// With createRoot React takes control of the HTML element (root) and with .render it renders inside it React component tree
+// With createRoot React takes control of the HTML container (root), where the app lives and with .render it renders inside it React component tree
 // with BrowserRouter components can access the browser's URL/navigation system
